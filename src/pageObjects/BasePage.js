1a6400bc55bc0df081fd6aa9492b1b5630ac0287
rysardP <rysard.poplavskij@ehu.lt>
2025-06-16 17:23:13 +0300

    EHU-ATJS-20296. Add "Universe" Field to the Previous Travel Information Section in the Petition for Space Travel Form

--- src/pageObjects/BasePage.js
+++ src/pageObjects/BasePage.js
@@ -6,7 +6,8 @@ export class BasePage {
     this.alertButtom = page.locator('button#trigger-alert');
     this.dateFieldLocatorByName = fieldName => page.getByRole('textbox', { name: `${fieldName}` })
     this.formField = fieldName => page.locator(`//label[contains(text(), "${fieldName}")]/following-sibling::input`);
-    this.fieldLocatorByName = fieldName => page.locator(`//label[contains(text(), "${fieldName}")]/following-sibling::input`);
+    this.fieldLocatorByName = fieldName =>
+      page.locator(`//label[contains(text(), "${fieldName}")]/following-sibling::input`);
   }
 
   async clickOnButton(name) {
